00ff8d79724d3d1e48fe6e2721a0df76ac08274b
Roch Devost <rochdevost@motivateco.com>
2018-03-23 10:33:54 -0400

    add update and get routes

--- src/router.js
+++ src/router.js
@@ -5,13 +5,32 @@ const docs = require('./docs')
 
 const router = express.Router()
 
+// Insert documents
 router.put('/docs', (req, res, next) => {
-  docs.insert(req.body, (err, newDoc) => {
+  docs.insert(req.body, (err) => {
     if (err) return next(err)
     res.status(200).send()
   })
 })
 
+// Update document by ID
+router.post('/docs/:id', (req, res, next) => {
+  docs.update({ _id: req.params.id }, req.body, {}, (err) => {
+    if (err) return next(err)
+    res.status(200).send()
+  })
+})
+
+// Get document by ID
+router.get('/docs/:id', (req, res, next) => {
+  docs.findOne({ _id: req.params.id }, (err, doc) => {
+    if (err) return next(err)
+    if (!doc) return res.status(404).send()
+    res.status(200).send(doc)
+  })
+})
+
+// Find documents
 router.get('/docs', (req, res, next) => {
   docs.find(req.query, (err, docs) => {
     if (err) return next(err)
