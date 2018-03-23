8bade2a4044ceb27513d21169aab5291095ebd1c
Roch Devost <rochdevost@motivateco.com>
2018-03-23 10:05:43 -0400

    fix error handling

--- src/router.js
+++ src/router.js
@@ -7,14 +7,14 @@ const router = express.Router()
 
 router.put('/docs', (req, res, next) => {
   docs.insert(req.body, (err, newDoc) => {
-    err && next(err)
+    if (err) return next(err)
     res.status(200).send()
   })
 })
 
 router.get('/docs', (req, res, next) => {
   docs.find(req.query, (err, docs) => {
-    err && next(err)
+    if (err) return next(err)
     res.status(200).send(docs)
   })
 })
